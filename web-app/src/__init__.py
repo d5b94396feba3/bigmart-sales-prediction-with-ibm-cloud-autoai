2e39ec43d3121034f07eca1a88a248917ef89b11
Shahibur Rahman <shahiburrahman55@gmail.com>
2021-05-01 12:07:08 +0600

    Update __init__.py

--- web-app/src/__init__.py
+++ web-app/src/__init__.py
@@ -1,7 +1,7 @@
 
 from flask import Flask
 from flask_mail import Mail, Message
-from itsdangerous import URLSafeTimedSerializer, SignatureExpired
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'khgfd45678jhfgg56678dfghjjhg457gffdghf64vhgd3463fghv4'
@@ -10,7 +10,6 @@ app.config.from_pyfile('config.cfg')
 
 mail = Mail(app)
 
-urlSTS = URLSafeTimedSerializer(app.config["SECRET_KEY"])
 
 def create_app():
 	return app
